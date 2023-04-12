00a5c266af249f964dff19c0f893109e6751d616
Ivanova Kseniia <ivanova.xe@mail.ru>
2023-04-12 11:46:36 +0300

    fixed by pep8

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -19,10 +19,12 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
     image = models.ImageField(upload_to="posts/", null=True, blank=True)
     group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL, related_name="posts", blank=True, null=True
+        Group, on_delete=models.SET_NULL, related_name="posts",
+        blank=True, null=True
     )
 
     class Meta:
@@ -33,10 +35,13 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     text = models.TextField()
-    created = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
+    created = models.DateTimeField("Дата добавления", auto_now_add=True,
+                                   db_index=True)
 
     class Meta:
         ordering = ("id",)
@@ -46,7 +51,8 @@ class Comment(models.Model):
 
 
 class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="follower")
     following = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="following"
     )
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -7,7 +7,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("redoc/", TemplateView.as_view(template_name="redoc.html"), name="redoc"),
+    path("redoc/", TemplateView.as_view(template_name="redoc.html"),
+         name="redoc"),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
